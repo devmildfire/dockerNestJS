3016e46e378f0471607d76a0682e25e8e92e3a8c
devmildfire <97608157+devmildfire@users.noreply.github.com>
2023-10-20 06:48:34 +0000

    add key and cert to main.ts

--- docker-nest/src/main.ts
+++ docker-nest/src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const fs = require('fs');
+  const keyFile  = fs.readFileSync(__dirname + '/certs/chain.pem');
+  const certFile = fs.readFileSync(__dirname + '/certs/cert.pem');
+  
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    }});
   await app.listen(3000);
 }
 bootstrap();
