f89547d5d9245cdf6c097c19467d1146bf368840
devmildfire <97608157+devmildfire@users.noreply.github.com>
2023-10-20 06:56:30 +0000

    fix

--- docker-nest/src/main.ts
+++ docker-nest/src/main.ts
@@ -3,14 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const fs = require('fs');
-  const keyFile  = fs.readFileSync(__dirname + '/certs/chain.pem');
-  const certFile = fs.readFileSync(__dirname + '/certs/cert.pem');
+  // const keyFile  = fs.readFileSync(__dirname + '../certs/chain.pem');
+  // const certFile = fs.readFileSync(__dirname + '../certs/cert.pem');
   
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: keyFile,
-      cert: certFile,
-    }});
+  // const keyFile  = fs.readFileSync('/certs/chain.pem');
+  // const certFile = fs.readFileSync('/certs/cert.pem');
+  
+  const app = await NestFactory.create(AppModule, 
+    // {    httpsOptions: {
+    //   key: keyFile,
+    //   cert: certFile,
+    // }}
+                                      );
   await app.listen(3000);
 }
 bootstrap();
