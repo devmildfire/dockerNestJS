29fc1578a9c951ac1587c9111198ab38b0da2508
devmildfire <97608157+devmildfire@users.noreply.github.com>
2023-10-20 06:35:29 +0000

    new path for mount

--- docker-nest/src/app.service.ts
+++ docker-nest/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World with push from gitHub actions and Docker - and New Mount maybe!';
+    return 'Hello World with push from gitHub actions and Docker - New Mount maybe!';
   }
 }
